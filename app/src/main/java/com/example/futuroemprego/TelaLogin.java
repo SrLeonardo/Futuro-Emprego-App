ebb721c71d9a9492a5a676b2af5441f9cb3fc2a0
SrLeonardo <60675717+SrLeonardo@users.noreply.github.com>
2021-01-16 04:02:51 -0300

    TelaCadastro

--- app/src/main/java/com/example/futuroemprego/TelaLogin.java
+++ app/src/main/java/com/example/futuroemprego/TelaLogin.java
@@ -2,13 +2,32 @@ package com.example.futuroemprego;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TelaLogin extends AppCompatActivity {
+
+    private TextView textCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+
+        textCadastro = findViewById(R.id.textCadastro);
+
+        textCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                Intent intent = new Intent(getApplicationContext(), TelaCadastro.class);
+
+                startActivity(intent);
+            }
+        });
+
     }
 }
