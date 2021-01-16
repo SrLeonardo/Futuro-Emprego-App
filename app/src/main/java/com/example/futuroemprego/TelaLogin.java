cb121e9382b3b9bf8a4e2f09e98d190a3ec93dea
SrLeonardo <60675717+SrLeonardo@users.noreply.github.com>
2021-01-16 18:15:44 -0300

    TelaCadastro alteração TelaLogin

--- app/src/main/java/com/example/futuroemprego/TelaLogin.java
+++ app/src/main/java/com/example/futuroemprego/TelaLogin.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TelaLogin extends AppCompatActivity {
 
@@ -24,15 +25,34 @@ public class TelaLogin extends AppCompatActivity {
         textCadastro = findViewById(R.id.textCadastro);
         loginEmail = findViewById(R.id.loginEmail);
         loginSenha = findViewById(R.id.loginSenha);
+        buttonLogar = findViewById(R.id.buttonLogar);
 
 
-       /* buttonLogar.setOnClickListener(new View.OnClickListener() {
+        buttonLogar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                String textoEmail = loginEmail.getText().toString();
+                String textoSenha = loginSenha.getText().toString();
 
+                if (!textoEmail.isEmpty()) {
+                    if ( !textoSenha.isEmpty()){
+
+
+
+
+                    }else{
+                        Toast.makeText( TelaLogin.this,
+                                "Preencha a Senha!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }else {
+                    Toast.makeText(TelaLogin.this,
+                            "Preencha o Email!",
+                            Toast.LENGTH_SHORT).show();
+                }
 
             }
-        });*/
+        });
 
 
 
