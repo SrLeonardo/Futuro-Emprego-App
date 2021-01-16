1992c061326f0ddfb31683c94b9fa15a6847c287
SrLeonardo <60675717+SrLeonardo@users.noreply.github.com>
2021-01-16 03:28:00 -0300

    TelaLogin Design

--- app/src/main/java/com/example/futuroemprego/MainActivity.java
+++ app/src/main/java/com/example/futuroemprego/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.futuroemprego;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonComecar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonComecar = findViewById(R.id.buttonComecar);
+
+        buttonComecar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                Intent intent = new Intent(getApplicationContext(), TelaLogin.class);
+
+                startActivity(intent);
+
+
+            }
+        });
+
     }
 }
