ef60d03e7dec27bb54171d0212edc697df562eb4
SrLeonardo <60675717+SrLeonardo@users.noreply.github.com>
2021-01-16 17:39:49 -0300

    TelaCadastro alteração TelaLogin

--- app/src/main/java/com/example/futuroemprego/TelaLogin.java
+++ app/src/main/java/com/example/futuroemprego/TelaLogin.java
@@ -6,11 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TelaLogin extends AppCompatActivity {
 
     private TextView textCadastro;
+    private EditText loginEmail, loginSenha;
+    private Button buttonLogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,34 @@ public class TelaLogin extends AppCompatActivity {
         setContentView(R.layout.activity_tela_login);
 
         textCadastro = findViewById(R.id.textCadastro);
+        loginEmail = findViewById(R.id.loginEmail);
+        loginSenha = findViewById(R.id.loginSenha);
+        buttonLogar = findViewById(R.id.buttonCadastro);
+
+        buttonLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String textoEmail = loginEmail.getText().toString();
+                String textoSenha = loginSenha.getText().toString();
+
+                if ( !textoEmail.isEmpty()){
+                    if( !textoSenha.isEmpty()){
+
+                    }else {
+                        Toast.makeText(TelaLogin.this,
+                                "Preencha a Senha!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                }else {
+                    Toast.makeText(TelaLogin.this,
+                            "Preencha o Email!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
         textCadastro.setOnClickListener(new View.OnClickListener() {
             @Override
