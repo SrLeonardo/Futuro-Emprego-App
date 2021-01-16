a9234d995c3a95ed9955c5203d920003b349c52c
SrLeonardo <60675717+SrLeonardo@users.noreply.github.com>
2021-01-16 04:43:27 -0300

    TelaCadastro - Database Import

--- app/src/main/java/com/example/futuroemprego/TelaCadastro.java
+++ app/src/main/java/com/example/futuroemprego/TelaCadastro.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class TelaCadastro extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
