a2466703d977174fb563cfed41f9501c33713e0c
SrLeonardo <60675717+SrLeonardo@users.noreply.github.com>
2021-01-17 21:48:36 -0300

    TelaHome nietzsche

---
+++ app/src/main/java/com/example/futuroemprego/TelaNietzsche.java
@@ -0,0 +1,14 @@
+package com.example.futuroemprego;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaNietzsche extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_nietzsche);
+    }
+}
